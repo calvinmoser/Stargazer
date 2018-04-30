4c6a864454342fada122d2cc5ff96c6042ddae3a
moserc <sysadmin@concordancehealth.com>
2018-04-30 13:43:42 -0400

    Add first star of first constellation and associated bg change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit{
-  circles = new Array();
+  stars = new Array();
+  constellations = new Array();
+  photo = '';
     
   ngOnInit() {
     for (let i = 0; i<100; i++) {
@@ -15,9 +17,26 @@ export class AppComponent implements OnInit{
         let y = 10000*Math.random();
         let r = 10*Math.random();
         let c = r * 25;
-        let color = `rgba(${c},${c},${c},1)`;
+        let gray = `rgba(${c},${c},${c},1)`;
+        let blue = `rgba(0,0,${c},1)`;
 
-        this.circles.push({ x: x, y: y, radius: r, color:color});
+        this.stars.push({ x: x, y: y, radius: r*1.2, color:blue});
+        this.stars.push({ x: x, y: y, radius: r, color:gray});
     }
+    let white = `rgba(255,255,255,1)`;
+    this.constellations.push({
+        x: 300,
+        y: 300,
+        radius: 20,
+        color:white});
+  }
+    
+  starClick(star) {
+    // change bg
+    if (this.photo == '')
+      this.photo = 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Ursa_Major_constellation_detail_map.PNG';
+    else
+        this.photo = '';
+
   }
 }
